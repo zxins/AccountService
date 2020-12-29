fb43ba1e6a2bbcce0eddb25641becd1ac8ba07fa
zhuxin <>
2020-12-29 10:09:56 +0800

    config

---
+++ config/config.template.js
@@ -0,0 +1,17 @@
+const Config = {
+    logger: {
+        "level": "info",
+        "prettyPrint": {colorize: true}
+    },
+    "timezone": "+08:00",
+    "database": {
+        "cache": {
+            "uri": "redis://127.0.0.1:6379/0",
+        },
+        "write": {
+            "uri": `mysql://root:${process.env.MYSQL_PASS}@127.0.0.1:3306/nodeBlog`,
+        },
+    },
+}
+
+module.exports = Config;
